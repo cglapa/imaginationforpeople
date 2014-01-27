56979584876a5a1fa96c296bc5f1c0db1ae92b78
Cyril Glapa <cyril.glapa@keuse.fr>
2014-01-27 14:20:48 +0100

    Moving search result cards to a dedicated template, and fixing bugs

--- apps/i4p_base/views.py
+++ apps/i4p_base/views.py
@@ -255,7 +255,7 @@ class SearchView(FacetedSearchView):
         project_sheet_tags = Tag.objects.usage_for_model(I4pProject.objects.translations_model, counts=True)
         project_sheet_tags.sort(key=lambda tag:-tag.count)
         context['project_sheet_tags']=project_sheet_tags
-        direct_objects = ['best_of', 'object']
+        direct_objects = ['id', 'best_of', 'object']
         translated_objects = ['title', 'baseline', 'about_section']
         for result in self.page.object_list:
             if(result):
